7162e6a80fc829d5deafb3b88cc6d3283d8c98ab
DuqueMajores <duque.majores@gmail.com>
2025-09-26 13:37:18 -0300

    reset db

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "uma-chave-secreta-muito-dificil"
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "sqlite:///site.db"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or "postgresql://freechat_db_user:MGsjJYsoxLNYpUnzmHmJdjowedZmnsOl@dpg-d3bbjjbe5dus73ce46dg-a.oregon-postgres.render.com/freechat_db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
